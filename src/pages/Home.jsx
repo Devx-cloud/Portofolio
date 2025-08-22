2c573c3b86e089a08a68b18522596bb34fc7ec61
Devx-cloud <devasuryagitpratama2006@gmail.com>
2025-08-22 21:12:52 +0800

    skillsSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { StarBackground } from "../components/StarBackground"
 import { Navbar } from "../components/Navbar"
 import { HomeSection } from "../components/HomeSection"
 import { AboutSection } from "../components/AboutSection"
+import { SkillSection } from "../components/SkillSection"
 
 export const Home = () =>{
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -18,8 +19,7 @@ export const Home = () =>{
         <main>
             <HomeSection/>
             <AboutSection/>
-            <AboutSection/>
-            <AboutSection/>
+            <SkillSection/>
         </main>
         {/* Footer */}
     </div>
