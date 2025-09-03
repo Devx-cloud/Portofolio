0c1571a0f54fe2b5563c56013f1cd44b12212967
Devx-cloud <devasuryagitpratama2006@gmail.com>
2025-09-03 20:06:14 +0800

    add contact and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { NotFound } from "./pages/NotFound"
+import { Contact } from "./pages/Contact"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home/>}/>
+          <Route path="/contact" element={<Contact />}/>
 
           <Route path="*" element={<NotFound/>}/>
         </Routes>
